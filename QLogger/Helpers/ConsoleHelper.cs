b83d63be14eded117588c76516ba0b86f0893067
Lincoln Yu <quanben@msn.com>
2015-09-20 11:34:26 +1000

    initial push.

---
+++ QLogger/Helpers/ConsoleHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace QLogger.Helpers
+{
+    public static class ConsoleHelper
+    {
+        #region Methods
+
+        public static int InplaceWrite(this int backwards, string msg)
+        {
+            for (var i = 0; i < backwards; i++)
+            {
+                Console.Out.Write('\b');
+            }
+            Console.Out.Write(msg);
+            return msg.Length;
+        }
+
+        public static int InplaceWrite(this int backwards, string format, params object[] args)
+        {
+            var s = string.Format(format, args);
+            return InplaceWrite(backwards, s);
+        }
+
+        public static void InplaceWriteLine(this int backwards, string msg)
+        {
+            for (var i = 0; i < backwards; i++)
+            {
+                Console.Out.Write('\b');
+            }
+            Console.Out.WriteLine(msg);
+        }
+
+        public static void InplaceWriteLine(this int backwards,
+            string format, params object[] args)
+        {
+            for (var i = 0; i < backwards; i++)
+            {
+                Console.Out.Write('\b');
+            }
+            Console.Out.WriteLine(format, args);
+        }
+
+        #endregion
+    }
+}
---
+++ QLogger/Logger.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using QLogger.Helpers;
+
+namespace QLogger
+{
+    public class Logger
+    {
+        #region Properties
+
+        public ISet<TextWriter> Writers { get; private set; } = new HashSet<TextWriter>();
+
+        #endregion
+
+        #region Methods
+
+        public void Write(string msg)
+        {
+            InplaceWrite(0, msg);
+        }
+
+        public void Write(string fmt, params object[] args)
+        {
+            InplaceWrite(0, fmt, args);
+        }
+
+        public void WriteLine(string msg)
+        {
+            InplaceWrite(0, msg);
+        }
+
+        public void WriteLine(string fmt, params object[] args)
+        {
+            InplaceWrite(0, fmt, args);
+        }
+
+        public int InplaceWrite(int back, string msg)
+        {
+            var res = 0;
+            foreach (var writer in Writers)
+            {
+                if (writer == Console.Out)
+                {
+                    res = back.InplaceWrite(msg);
+                }
+                else
+                {
+                    writer.Write(msg);
+                }
+            }
+            return res;
+        }
+
+        public int InplaceWrite(int back, string fmt, params object[] args)
+        {
+            var res = 0;
+            foreach (var writer in Writers)
+            {
+                if (writer == Console.Out)
+                {
+                    res = back.InplaceWrite(fmt, args);
+                }
+                else
+                {
+                    writer.Write(fmt, args);
+                }
+            }
+            return res;
+        }
+
+        public void InplaceWriteLine(int back, string msg)
+        {
+            foreach (var writer in Writers)
+            {
+                if (writer == Console.Out)
+                {
+                    back.InplaceWriteLine(msg);
+                }
+                else
+                {
+                    writer.WriteLine(msg);
+                }
+            }
+        }
+
+        public void InplaceWriteLine(int back, string fmt, params object[] args)
+        {
+            foreach (var writer in Writers)
+            {
+                if (writer == Console.Out)
+                {
+                    back.InplaceWriteLine(fmt, args);
+                }
+                else
+                {
+                    writer.WriteLine(fmt, args);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
