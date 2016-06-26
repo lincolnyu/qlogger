7115e277a393ee9a49faac9e662c11503ea6ff26
quanb <quanb@QUANBEN-ASUS>
2016-06-26 18:40:21 +1000

    added AppInfo

---
+++ QLogger/AppHelpers/AppInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Deployment.Application;
+
+namespace QLogger.AppHelpers
+{
+    public static class AppInfo
+    {
+        public static string GetAppExecutableName()
+        {
+            return Assembly.GetEntryAssembly().GetName().Name;
+        }
+
+        public static Version GetAppVersion()
+        {
+            try
+            {
+                return ApplicationDeployment.CurrentDeployment.CurrentVersion;
+            }
+            catch (InvalidDeploymentException)
+            {
+                return Assembly.GetEntryAssembly().GetName().Version;
+            }
+        }
+    }
+}
