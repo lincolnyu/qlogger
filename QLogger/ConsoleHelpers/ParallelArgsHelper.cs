8c1fc786749460abb61f8f9180cfb016774b673f
quanb <quanb@QUANBEN-ASUS>
2016-08-21 18:39:15 +1000

    i like parallelism

---
+++ QLogger/ConsoleHelpers/ParallelArgsHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace QLogger.ConsoleHelpers
+{
+    public static class ParallelArgsHelper
+    {
+        public static int GetMaxDegreeOfParallelism(this string[] args, string parallelSwitch = "-p")
+        {
+            var parallelarg = args.GetSwitchValue(parallelSwitch);
+            if (parallelarg == null) return 1;
+            int parallel;
+            int.TryParse(parallelarg, out parallel);
+            return parallel;
+        }
+    }
+}
---
+++ QLogger/Parallelism/Parallelizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace QLogger.Parallelism
+{
+    // This is the base class that provides the context for parallelizer to inherit
+    // so it doesn't need to worry about incorporating the info in its arguments and
+    // can have a conformant signature as its serial counterpart
+    public abstract class Parallelizer
+    {
+        public Parallelizer(int maxDegreeOfParallelism = int.MaxValue)
+        {
+            ParallelOptions = new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism };
+        }
+
+        public Parallelizer(ParallelOptions options)
+        {
+            ParallelOptions = options;
+        }
+
+        public ParallelOptions ParallelOptions { get; }
+    }
+}
